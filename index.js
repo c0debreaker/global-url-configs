d0e2177b4f18c41b15e6a82c5cf1c6466f7ab616
Ronneil Camara <rcamara@rocketmail.com>
2017-04-15 16:34:42 -0500

    Added few properties

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ module.exports = {
     "medicontentServer": "http://medicontent.example.com",
     "apiServer": "https://api.example.com",
     "custhelpServer": "https://custhelp.example.com"
+  },
+  "endpoints": {
+    "add": "/api/v1/adduser",
+    "delete": "/api/v1/deluser",
+    "update": "/api/v1/updateuser"
   }
 }
